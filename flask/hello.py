3726bcbbaa128368dba3621447d26bd7699e6737
Sai <svv232@nyu.edu>
2017-10-22 21:49:45 -0400

    Added an About Template/About to start JS

--- flask/hello.py
+++ flask/hello.py
@@ -10,5 +10,9 @@ app = Flask(__name__, static_url_path=str(os.path.dirname(__file__)))
 def index():
     return render_template("index.html")
 
+@app.route("/about-me")
+def about():
+    return render_template("about-me.html")
+
 if __name__ == "__main__":
     app.run()
